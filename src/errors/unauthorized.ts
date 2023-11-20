516612750235dd1d71ded7768722e1e342159df8
Joel Emmanuel <ejoel0035@gmail.com>
2023-11-20 14:40:23 +0100

    feat: add unauthorized error

---
+++ src/errors/unauthorized.ts
@@ -0,0 +1,12 @@
+import { StatusCodes } from 'http-status-codes';
+import CustomAPIError from './custom-api';
+
+class UnauthorizedError extends CustomAPIError {
+  statusCode: StatusCodes;
+  constructor(message: string) {
+    super(message);
+    this.statusCode = StatusCodes.FORBIDDEN;
+  }
+}
+
+export default UnauthorizedError;
