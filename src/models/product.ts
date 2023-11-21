1f60651643298c64dc02226d2bd49e1f9e92f726
Joel Emmanuel <ejoel0035@gmail.com>
2023-11-21 04:39:00 +0100

    feat: add product model

---
+++ src/models/product.ts
@@ -0,0 +1,105 @@
+import mongoose, { Document, PopulatedDoc, Schema } from 'mongoose';
+import { IUser } from './user'; 
+import { IReview } from './reviews';
+
+export interface IProduct extends Document {
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+  category: 'office' | 'kitchen' | 'bedroom';
+  company: 'ikea' | 'liddy' | 'marcos';
+  colors: string[];
+  featured: boolean;
+  freeShipping: boolean;
+  inventory: number;
+  averageRating: number;
+  numOfReviews: number;
+  user: PopulatedDoc<IUser & Document>;
+  reviews: PopulatedDoc<IReview & Document>[];
+}
+
+const ProductSchema = new Schema<IProduct>(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, 'Please provide product name'],
+      maxlength: [100, 'Name can not be more than 100 characters'],
+    },
+    price: {
+      type: Number,
+      required: [true, 'Please provide product price'],
+      default: 0,
+    },
+    description: {
+      type: String,
+      required: [true, 'Please provide product description'],
+      maxlength: [1000, 'Description can not be more than 1000 characters'],
+    },
+    image: {
+      type: String,
+      default: '/uploads/example.jpeg',
+    },
+    category: {
+      type: String,
+      required: [true, 'Please provide product category'],
+      enum: ['office', 'kitchen', 'bedroom'],
+    },
+    company: {
+      type: String,
+      required: [true, 'Please provide company'],
+      enum: {
+        values: ['ikea', 'liddy', 'marcos'],
+        message: '{VALUE} is not supported',
+      },
+    },
+    colors: {
+      type: [String],
+      default: ['#222'],
+      required: true,
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+    freeShipping: {
+      type: Boolean,
+      default: false,
+    },
+    inventory: {
+      type: Number,
+      required: true,
+      default: 15,
+    },
+    averageRating: {
+      type: Number,
+      default: 0,
+    },
+    numOfReviews: {
+      type: Number,
+      default: 0,
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User', 
+      required: true,
+    },
+  },
+  { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
+);
+
+ProductSchema.virtual('reviews', {
+  ref: 'Review', 
+  localField: '_id',
+  foreignField: 'product',
+  justOne: false,
+});
+
+ProductSchema.pre('remove', async function (next) {
+  await this.model('Review').deleteMany({ product: this._id });
+});
+
+const Product = mongoose.model<IProduct>('Product', ProductSchema);
+
+export default Product;
